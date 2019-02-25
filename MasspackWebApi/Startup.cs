1bf31dfee4ccb3f1a4750fcbc89f74edae05e07e
bajwaimran <bajwa.imran@icloud.com>
2019-02-25 11:01:33 +0100

    Add project files.

---
+++ MasspackWebApi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MasspackWebApi.Startup))]
+namespace MasspackWebApi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
